402ed55bc42d58f8e233903d60beced5ef9dc60b
jregan <jregan@google.com>
2020-02-01 19:47:10 -0800

    simplify

--- lib/conway_world.dart
+++ lib/conway_world.dart
@@ -216,11 +216,3 @@ class ConwayWorld {
     }
   }
 }
-
-main() {
-  ConwayWorld.movie(ConwayWorld.blinker, 80);
-  ConwayWorld.movie(ConwayWorld.pentaDecathlon, 80);
-  ConwayWorld.movie(ConwayWorld.lightweightSpaceship.padRight(30).padBottom(1), 80);
-  ConwayWorld.movie(ConwayWorld.glider.padRight(30).padBottom(30), 80);
-  ConwayWorld.movie(ConwayWorld.gunFight(), 1000);
-}
--- lib/rectangle_world.dart
+++ lib/rectangle_world.dart
@@ -70,7 +70,7 @@ class RectangleWorld {
     }
     final nR = lines.length;
     final nC = lines[0].length;
-    for (var i = 1; i < lines.length; i++) {
+    for (int i = 1; i < lines.length; i++) {
       if (lines[i].length != nC) {
         throw 'all lines must be same length';
       }
@@ -85,15 +85,23 @@ class RectangleWorld {
   }
 
   factory RectangleWorld.empty(int nR, int nC) {
-    return RectangleWorld(nR, nC, List<bool>(nR * nC));
+    return RectangleWorld(nR, nC, List<bool>.filled(nR * nC, false));
+  }
+
+  factory RectangleWorld.identity(int nR) {
+    final rw = RectangleWorld.empty(nR, nR);
+    for (int i = 0; i < nR; i++) {
+      rw._cells[rw.index(i, i)] = true;
+    }
+    return rw;
   }
 
   // Copy this as a transpose.
   RectangleWorld transpose() {
     final newCells = List<bool>(_numRows * _numCols);
     final newIndex = (int j, int i) => (j * _numRows) + i;
-    for (var i = 0; i < _numRows; i++) {
-      for (var j = 0; j < _numCols; j++) {
+    for (int i = 0; i < _numRows; i++) {
+      for (int j = 0; j < _numCols; j++) {
         newCells[newIndex(j, i)] = isAlive(i, j);
       }
     }
@@ -104,13 +112,14 @@ class RectangleWorld {
   // world's {0,0} at this world's {cI,cJ}.  This world won't grow
   // to fit.  If incoming world is too big or too far 'down' or 'right'
   // it will overwrite cells due to boundary wrapping.
-  void paste(final int cI, final int cJ, final RectangleWorld rw) {
-    for (var i = 0; i < rw.nRows; i++) {
+  RectangleWorld paste(final int cI, final int cJ, final RectangleWorld rw) {
+    for (int i = 0; i < rw.nRows; i++) {
       final int tI = (cI + i) % _numRows;
-      for (var j = 0; j < rw.nCols; j++) {
+      for (int j = 0; j < rw.nCols; j++) {
         _cells[index(tI, (cJ + j) % _numCols)] = rw.isAlive(i, j);
       }
     }
+    return this;
   }
 
   // Like paste, but a new world is returned big enough to cleanly
@@ -119,17 +128,15 @@ class RectangleWorld {
       final int cI, final int cJ, final RectangleWorld incoming) {
     RectangleWorld rw = RectangleWorld.empty(
         max(_numRows, cI + incoming.nRows), max(_numCols, cJ + incoming.nCols));
-    rw.paste(0, 0, this);
-    rw.paste(cI, cJ, incoming);
-    return rw;
+    return rw.paste(0, 0, this).paste(cI, cJ, incoming);
   }
 
   // Copy this as a clockwise 90 degree rotation.
   RectangleWorld clockwise90() {
     final newCells = List<bool>(_numRows * _numCols);
     final newIndex = (int i, int j) => (j * _numRows) + (_numRows - 1 - i);
-    for (var i = 0; i < _numRows; i++) {
-      for (var j = 0; j < _numCols; j++) {
+    for (int i = 0; i < _numRows; i++) {
+      for (int j = 0; j < _numCols; j++) {
         newCells[newIndex(i, j)] = isAlive(i, j);
       }
     }
@@ -140,8 +147,8 @@ class RectangleWorld {
   RectangleWorld counterClockwise90() {
     final newCells = List<bool>(_numRows * _numCols);
     final newIndex = (int i, int j) => ((_numCols - 1 - j) * _numRows) + i;
-    for (var i = 0; i < _numRows; i++) {
-      for (var j = 0; j < _numCols; j++) {
+    for (int i = 0; i < _numRows; i++) {
+      for (int j = 0; j < _numCols; j++) {
         newCells[newIndex(i, j)] = isAlive(i, j);
       }
     }
@@ -150,68 +157,26 @@ class RectangleWorld {
 
   // Copy this, adding padding on left.
   RectangleWorld padLeft(int n) {
-    final newNumCols = _numCols + n;
-    final newCells = List<bool>(_numRows * newNumCols);
-    final newIndex = (int i, int j) => (i * newNumCols) + j;
-    for (var i = 0; i < _numRows; i++) {
-      for (var j = 0; j < n; j++) {
-        newCells[newIndex(i, j)] = false;
-      }
-      for (var j = 0; j < _numCols; j++) {
-        newCells[newIndex(i, j + n)] = isAlive(i, j);
-      }
-    }
-    return RectangleWorld(_numRows, newNumCols, newCells);
+    var rw = RectangleWorld.empty(_numRows, _numCols + n);
+    return rw.paste(0, n, this);
   }
 
   // Copy this, adding padding on right.
   RectangleWorld padRight(int n) {
-    final newNumCols = _numCols + n;
-    final newCells = List<bool>(_numRows * newNumCols);
-    final newIndex = (int i, int j) => (i * newNumCols) + j;
-    for (var i = 0; i < _numRows; i++) {
-      for (var j = 0; j < _numCols; j++) {
-        newCells[newIndex(i, j)] = isAlive(i, j);
-      }
-      for (var j = _numCols; j < newNumCols; j++) {
-        newCells[newIndex(i, j)] = false;
-      }
-    }
-    return RectangleWorld(_numRows, newNumCols, newCells);
+    var rw = RectangleWorld.empty(_numRows, _numCols + n);
+    return rw.paste(0, 0, this);
   }
 
   // Copy this, adding padding on top.
   RectangleWorld padTop(int n) {
-    final newNumRows = _numRows + n;
-    final newCells = List<bool>(newNumRows * _numCols);
-    for (var i = 0; i < n; i++) {
-      for (var j = 0; j < _numCols; j++) {
-        newCells[index(i, j)] = false;
-      }
-    }
-    for (var i = 0; i < _numRows; i++) {
-      for (var j = 0; j < _numCols; j++) {
-        newCells[index(i + n, j)] = isAlive(i, j);
-      }
-    }
-    return RectangleWorld(newNumRows, _numCols, newCells);
+    var rw = RectangleWorld.empty(_numRows + n, _numCols);
+    return rw.paste(n, 0, this);
   }
 
   // Copy this, adding padding on bottom.
   RectangleWorld padBottom(int n) {
-    final newNumRows = _numRows + n;
-    final newCells = List<bool>(newNumRows * _numCols);
-    for (var i = 0; i < _numRows; i++) {
-      for (var j = 0; j < _numCols; j++) {
-        newCells[index(i, j)] = isAlive(i, j);
-      }
-    }
-    for (var i = _numRows; i < newNumRows; i++) {
-      for (var j = 0; j < _numCols; j++) {
-        newCells[index(i, j)] = false;
-      }
-    }
-    return RectangleWorld(newNumRows, _numCols, newCells);
+    var rw = RectangleWorld.empty(_numRows + n, _numCols);
+    return rw.paste(0, 0, this);
   }
 
   // Append the other world to the right of this one.
@@ -219,26 +184,7 @@ class RectangleWorld {
   // shorter of the two.
   // No attempt to center the shorter one.
   RectangleWorld appendRight(RectangleWorld other) {
-    RectangleWorld left = this, right = other;
-    if (other._numRows > _numRows) {
-      left = this.padBottom(other._numRows - _numRows);
-    } else if (_numRows > other._numRows) {
-      right = other.padBottom(_numRows - other._numRows);
-    }
-    assert(left._numRows == right._numRows);
-    final newNumCols = left._numCols + right._numCols;
-    final newNumRows = left._numRows;
-    final newCells = List<bool>(newNumRows * newNumCols);
-    final newIndex = (int i, int j) => (i * newNumCols) + j;
-    for (var i = 0; i < newNumRows; i++) {
-      for (var j = 0; j < left._numCols; j++) {
-        newCells[newIndex(i, j)] = left.isAlive(i, j);
-      }
-      for (var j = 0; j < right._numCols; j++) {
-        newCells[newIndex(i, j + left._numCols)] = right.isAlive(i, j);
-      }
-    }
-    return RectangleWorld(newNumRows, newNumCols, newCells);
+    return expandToPaste(0, _numCols, other);
   }
 
   // Append the other world to the bottom of this one.
@@ -246,41 +192,6 @@ class RectangleWorld {
   // thinner of the two.
   // No attempt to center the thinner one.
   RectangleWorld appendBottom(RectangleWorld other) {
-    RectangleWorld top = this, bottom = other;
-    if (other._numCols > _numCols) {
-      top = this.padRight(other._numCols - _numCols);
-    } else if (_numCols > other._numCols) {
-      bottom = other.padRight(_numCols - other._numCols);
-    }
-    assert(top._numCols == bottom._numCols);
-    final newNumCols = top._numCols;
-    final newNumRows = top._numRows + bottom._numRows;
-    final newCells = List<bool>(newNumRows * newNumCols);
-    final newIndex = (int i, int j) => (i * newNumCols) + j;
-    for (var i = 0; i < top._numRows; i++) {
-      for (var j = 0; j < newNumCols; j++) {
-        newCells[newIndex(i, j)] = top.isAlive(i, j);
-      }
-    }
-    for (var i = 0; i < bottom._numRows; i++) {
-      for (var j = 0; j < newNumCols; j++) {
-        newCells[newIndex(i + top.nRows, j)] = bottom.isAlive(i, j);
-      }
-    }
-    return RectangleWorld(newNumRows, newNumCols, newCells);
+    return expandToPaste(_numRows, 0, other);
   }
-}
-
-main() {
-  var r1 = RectangleWorld.fromString('''
-..#..
-..##.
-..###
-..#..
-..#..
-..#..
-''').padLeft(2).padTop(2).padBottom(3).padRight(10);
-  var r2 = r1.clockwise90().clockwise90();
-  var w = r1.appendBottom(r2);
-  print(w.toString());
 }
